52abbd6295d012b6540c9b86b0b962d954d8ed7a
seigtm <seigtm@gmail.com>
2022-01-25 17:49:24 +0300

    Updated and sorted common includes.

--- src/Common.hpp
+++ src/Common.hpp
@@ -7,8 +7,12 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
 
+#include <glm/glm.hpp>
+
+#include <algorithm>
+#include <bitset>
+#include <chrono>
 #include <cstdio>
+#include <functional>
 #include <string>
 #include <string_view>
-#include <algorithm>
-#include <functional>
