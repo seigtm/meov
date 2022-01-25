a341ebfaa64a88e829fc287f7c1e9b68cdc17a26
seigtm <seigtm@gmail.com>
2022-01-25 17:53:30 +0300

    Used new window classes in main src.

--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,34 @@
 #include "Common.hpp"
 #include "AppInfo.hpp"
 
-// Main code
+#include "windows/Git.hpp"
+#include "windows/Test.hpp"
+
 int main(int, char**) {
+    // Init SDL.
     if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
         printf("Error: %s\n", SDL_GetError());
         return -1;
     }
-
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, MEOV::AppInfo::GLSLVersionMajor());
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, MEOV::AppInfo::GLSLVersionMinor());
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-    // Create window with graphics context
+    // Create window with graphics context.
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    SDL_Window* window { SDL_CreateWindow(
+    SDL_Window* window{ SDL_CreateWindow(
         MEOV::AppInfo::Name().c_str(),
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         1280, 720,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI
-    )};
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI) };
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
     SDL_GL_MakeCurrent(window, gl_context);
-    SDL_GL_SetSwapInterval(1);  // Enable vsync
+    SDL_GL_SetSwapInterval(1);  // Enable vsync.
 
-    // Setup Dear ImGui context
+    // Setup Dear ImGui context.
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
@@ -37,26 +38,27 @@ int main(int, char**) {
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
 
-    // Setup Dear ImGui style
+    // Setup Dear ImGui style.
     ImGui::StyleColorsDark();
 
-    // Setup Platform/Renderer backends
+    // Setup Platform/Renderer backends.
     ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
     ImGui_ImplOpenGL3_Init(MEOV::AppInfo::GLSLVersion().c_str());
 
-    // Our state
+    // Our state.
     bool show_demo_window = true;
-    bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-    // Main loop
+    // Dear ImGui windows.
+    MEOV::Window::Git gitW;
+    MEOV::Window::Test testW;
+    testW.ToggleNoResize();
+    testW.ToggleNoMove();
+
+    // Main loop.
     bool done = false;
     while(!done) {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        // Poll and handle events (inputs, window resize, etc.).
         SDL_Event event;
         while(SDL_PollEvent(&event)) {
             ImGui_ImplSDL2_ProcessEvent(&event);
@@ -71,50 +73,15 @@ int main(int, char**) {
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if(show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        // Show the big demo window.
+        ImGui::ShowDemoWindow(&show_demo_window);
 
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
+        // Show Git info window.
+        gitW.Draw();
+        // Show another simple test window.
+        testW.Draw();
 
-            ImGui::Begin("Hello, world!");  // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");           // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);  // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color);  // Edit 3 floats representing a color
-
-            if(ImGui::Button("Button"))  // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
-        }
-        {
-            ImGui::Begin("Git information");
-            ImGui::Text("Git hash:    %s", MEOV::AppInfo::GitCommitHash().c_str());
-            ImGui::Text("Git message: %s", MEOV::AppInfo::GitCommitMessage().c_str());
-            ImGui::Text("Git date:    %s", MEOV::AppInfo::GitCommitDate().c_str());
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if(show_another_window) {
-            ImGui::Begin("Another Window", &show_another_window);  // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if(ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
-
-        // Rendering
+        // Rendering.
         ImGui::Render();
         glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
         glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
@@ -123,7 +90,7 @@ int main(int, char**) {
         SDL_GL_SwapWindow(window);
     }
 
-    // Cleanup
+    // Cleanup.
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplSDL2_Shutdown();
     ImGui::DestroyContext();
