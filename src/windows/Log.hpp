5a360f243410970c0e6190cb9b8266bb32b3f1bf
seigtm <seigtm@gmail.com>
2022-02-27 13:57:39 +0300

    Resizable log window through the constructor.

--- src/windows/Log.hpp
+++ src/windows/Log.hpp
@@ -10,7 +10,7 @@ namespace meov::Window {
 class Log final : public Base, public Utils::Log::Storage::Subscriber {
 public:
     using Reference = std::shared_ptr<Log>;  // FIXME: ambiguous '::Ref' from Subscriber.
-    Log(std::string_view const title);
+    Log(std::string_view const title, ImVec2 const& size = { 650, 200 });
 
 protected:
     void DrawImpl() override final;
