247848f253b019b4a5a77023565d124149c4d34d
seigtm <gh@seig.ru>
2022-04-11 15:51:44 +0300

    SDL and GLBinding as submodules instead of conan packages.

--- cmake/conan_dependencies.cmake
+++ cmake/conan_dependencies.cmake
@@ -1,21 +1,6 @@
 macro(conan_install)
-    # If not multi-config generator (like VS generator).
-    set(_options
-        sdl:vulkan=False
-        sdl:sdl2main=False
-    )
-
-    if("${CMAKE_CONFIGURATION_TYPES}" STREQUAL "")
-        if(NOT ${CMAKE_GENERATOR} STREQUAL "Ninja")
-            list(APPEND _options sdl:iconv=False)
-        endif()
-    endif()
-
     conan_cmake_configure(
         REQUIRES
-            sdl/2.0.18
-            glbinding/3.1.0
-            glew/2.2.0
             plog/1.1.5
             assimp/5.1.0
         OPTIONS
